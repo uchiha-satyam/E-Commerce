1f4bf7325583d5ddff701403485aab748e66d10a
Satyam Abhishek <satyam.abhi02@gmail.com>
2022-05-22 16:12:35 +0530

    Add navigate to redirect to home if signed in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { connect } from 'react-redux';
 import './App.css';
 
@@ -42,15 +42,25 @@ class App extends React.Component {
         <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='/shop' element={<ShopPage />} />
-          <Route path='/sign' element={<SignInAndSignUpPage />} />
+          <Route path='/sign' element={
+            this.props.currentUser ? (
+              <Navigate to='/' replace />
+            ) : (
+              <SignInAndSignUpPage />
+            )
+          } />
         </Routes>
       </div>
     )
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
