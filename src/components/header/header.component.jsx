677fbb9959b042ea13a51264c3de8319df210fe2
Satyam Abhishek <satyam.abhi02@gmail.com>
2022-05-25 20:55:05 +0530

    Create cart-item

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -45,7 +45,7 @@ const Header = ({ currentUser, hidden }) => (
 	</div>
 )
 
-const mapStateToProps = ({ user: { currentUser }, cart: { hidden }}) => ({
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
 	currentUser,
 	hidden
 })
