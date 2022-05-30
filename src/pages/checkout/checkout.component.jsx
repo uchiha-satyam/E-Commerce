662e4fa845cae80d91cfb721db16e54563ee2807
Satyam Abhishek <satyam.abhi02@gmail.com>
2022-05-30 09:30:42 +0530

    Integrate Razorpay frontend

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import RazorpayCheckoutButton from '../../components/razorpay-button/razorpay-button.component';
 import { selectCartItems, selectCartAmount } from '../../redux/cart/cart.selectors';
 import './checkout.styles.scss';
 
@@ -30,6 +31,16 @@ const CheckoutPage = ({ cartItems, amount }) => (
 		}
 		<div className="total">
 			<span>To Pay : ₹ {amount}</span>
+		</div>
+		{
+			amount ? <RazorpayCheckoutButton price={amount} /> : null
+		}
+		<div className="test-warning">
+			*Please use the following TEST CARDS for payment*
+			<br />
+			<span>MasterCard -&gt; 5267 3181 8797 5449 - Random CVV - Any future date</span>
+			<br />
+			<span>Visa&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; -&gt; 4111 1111 1111 1111 - Random CVV - Any future date</span>
 		</div>
 	</div>
 )
