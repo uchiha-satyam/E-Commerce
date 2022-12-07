1c1b14ca7838ed684bd4ecf917400f3f090e9d5c
Satyam Abhishek <satyam.abhi02@gmail.com>
2022-12-07 10:19:44 +0530

    Change add to cart button when user not signed in

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { createStructuredSelector } from 'reselect';
 
 import { addItem } from '../../redux/cart/cart.actions';
+import { selectCurrentUser } from '../../redux/user/user.selectors';
 import { CollectionItemContainer, CollectionFooterContainer, ImageContainer ,CustomButtonContainer, NameContainer, PriceContainer } from './collection-item.styles';
 
-const CollectionItem = ({item, addItem}) => {
+const CollectionItem = ({item, addItem, currentUser}) => {
 	const { name, price, imageUrl } = item;
+	const navigate = useNavigate();
 
 	return (
 		<CollectionItemContainer>
@@ -19,18 +23,32 @@ const CollectionItem = ({item, addItem}) => {
 				<NameContainer>{name}</NameContainer>
 				<PriceContainer>₹{price}</PriceContainer>
 			</CollectionFooterContainer>
-			<CustomButtonContainer
-				onClick={() => addItem(item)}
-				inverted
-			>
-				Add To Cart
-			</CustomButtonContainer>
+			{
+				currentUser?
+				<CustomButtonContainer
+					onClick={() => addItem(item)}
+					inverted
+				>
+					Add To Cart
+				</CustomButtonContainer>
+				:
+				<CustomButtonContainer
+					onClick={() => navigate('/sign')}
+					inverted
+				>
+					Sign in first
+				</CustomButtonContainer>
+			}
 		</CollectionItemContainer>
 	)
 }
+
+const mapStateToProps = createStructuredSelector({
+	currentUser: selectCurrentUser
+})
 
 const mapDispatchToProps = dispatch => ({
 	addItem: item => dispatch(addItem(item))
 })
 
-export default connect(null, mapDispatchToProps)(CollectionItem);
+export default connect(mapStateToProps, mapDispatchToProps)(CollectionItem);
